74b06444dd4c798f486e710c453f052528a1a2f4
Shubham yadav <56728522+Shubhamyadav03@users.noreply.github.com>
2022-08-10 10:07:29 +0530

    backend

--- Capstone12 - Copy/src/app/result.ts
+++
@@ -1,9 +0,0 @@
-export class Result {
-    studentId:number;
-    result:string;
-    constructor( studentId:number,result:string)
-    {
-        this.studentId=studentId;
-        this.result=result;
-    }
-}
